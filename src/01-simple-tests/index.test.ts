f1a5c4bdf287e9a1ab5df8716c91a8f00ee73b53
antonina220590 <tonya2290@gmail.com>
2025-05-07 18:41:22 +0300

    feat: add tests for 01_simple_tests file

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,64 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const inputForAdd = { a: 2, b: 2, action: Action.Add };
+    const expectedResult = 4;
+    const actualResult = simpleCalculator(inputForAdd);
+    expect(actualResult).toBe(expectedResult);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const inputForSubtract = { a: 5, b: 2, action: Action.Subtract };
+    const expectedResult = 3;
+
+    const actualResult = simpleCalculator(inputForSubtract);
+    expect(actualResult).toBe(expectedResult);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const inputForMultiply = { a: 2, b: 2, action: Action.Multiply };
+    const expectedResult = 4;
+
+    const actualResult = simpleCalculator(inputForMultiply);
+    expect(actualResult).toBe(expectedResult);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const inputForDivide = { a: 20, b: 2, action: Action.Divide };
+    const expectedResult = 10;
+
+    const actualResult = simpleCalculator(inputForDivide);
+    expect(actualResult).toBe(expectedResult);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const inputForExponentiate = { a: 2, b: 2, action: Action.Exponentiate };
+    const expectedResult = 4;
+
+    const actualResult = simpleCalculator(inputForExponentiate);
+    expect(actualResult).toBe(expectedResult);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const inputWithInvalidAction = {
+      a: 5,
+      b: 3,
+      action: 'invalid_operation_string',
+    };
+    const actualResult = simpleCalculator(inputWithInvalidAction);
+    expect(actualResult).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const inputWithInvalidArgs = {
+      a: 'invalid_a',
+      b: 'invalid_b',
+      action: Action.Divide,
+    };
+
+    const actualResult = simpleCalculator(inputWithInvalidArgs);
+    expect(actualResult).toBeNull();
   });
 });
